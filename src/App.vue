42961c8ef02b6c8f12047d9c7545fb363bffd8f9
chenw <3445883193@qq.com>
2024-02-13 19:35:55 +0800

    firstcommit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const str = ' woaini'
+console.log(str)
+</script>
+
+<template>
+  <div class="box">
+    <h1>我是app</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background-color: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
